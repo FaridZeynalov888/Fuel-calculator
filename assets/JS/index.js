bb83c6ab8a37ced2f9ffb02977a7d13143ff82e4
FaridZeynalov888 <farid.zeynalovc96@gmail.com>
2023-08-05 10:24:21 +0400

    init first commit

---
+++ assets/JS/index.js
@@ -0,0 +1,19 @@
+// ? Task 1
+{
+    function carTest(fuel,distance){
+        a = fuel;
+        b = distance;
+        result = (fuel / (distance / 100))
+        alert("Result: " + result);
+    }
+    carTest(50,200);
+}
+// ? Task 2
+{
+    let a = prompt("Enter the number");
+
+    a % 2 == 0 ? 
+    alert("The number you entered is even") : 
+    alert("The number you entered is odd");
+}
+
